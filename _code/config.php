0f9d9ea4b8014cf3c1166069cf9829ddd7008baf
doubotis <doubotis@gmail.com>
2014-04-27 14:53:30 +0200

    Update 3
    Updating config.php

---
+++ _code/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *  CONFIG FILE FOR TRANSMOGRIFY
+ *  This is your config file. Make sure permissions is set to 0644 or 0444 !
+ */
+// Config Transmogrify
+$config["host"] = "localhost";
+$config["db_auth_username"] = "<your_username_here>";
+$config["db_auth_password"] = "<your_password_here>";
+$config["db_auth_dbname"] = "<your_db_name_here>";
+
+?>
